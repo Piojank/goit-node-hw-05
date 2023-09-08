7cc9109b6224c3c02d1d3fbfcb52c802d081380e
piojank <p.jankowski931026@gmail.com>
2023-09-08 21:04:21 +0200

    Initialize Repository

---
+++ server.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+require("dotenv").config();
+mongoose.Promise = global.Promise;
+
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.DB_HOST;
+
+const connection = mongoose.connect(uriDb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: process.env.DB_NAME,
+});
+
+connection
+    .then(() => {
+        console.log("Database connection successful");
+        app.listen(PORT, () => {
+            console.log(`App listens on port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error(`Error while establishing connection: [${error}]`);
+        process.exit(1);
+    });
