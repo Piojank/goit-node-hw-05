de2a49580d1fbd6a97e15110213739a0a9cf94fc
piojank <p.jankowski931026@gmail.com>
2023-09-15 00:39:05 +0200

    add mutlerController

--- config/config-multer.js
+++ config/config-multer.js
@@ -1,24 +1,19 @@
 const multer = require("multer");
 const path = require("path");
 
-const uploadDir = path.join(process.cwd(), "public/tmp");
+const tempDir = path.join(process.cwd(), "public/tmp");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, uploadDir);
+        cb(null, tempDir)
     },
     filename: (req, file, cb) => {
-        getFilenameWithSuffixCb(file.originalname, cb);
+        cb(null, file.originalname)
     },
     limits: {
         fileSize: 1048576,
     },
 });
-
-const getFilenameWithSuffixCb = (originalname, callback) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e3);
-    callback(null, uniqueSuffix + "-" + originalname);
-};
 
 const upload = multer({
     storage: storage,
